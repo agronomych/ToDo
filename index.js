e1d1e9e8a458a3e30d0344f3b4e82c2fc0bb44d8
Agronomych <anton.suryapin@gmail.com>
2018-03-26 15:31:56 +0300

    '1'

--- index.js
+++ index.js
@@ -3,6 +3,22 @@
  */
 var http = require('http');
 var fs = require('fs');
+
+function saveData(data){
+    console.log('Saving data...');
+    fs.writeFile("./pages/toDoList.txt",data,function (error) {
+        if (error) throw error;
+    });
+}
+
+function loadData(){
+    console.log('Loading data...');
+    data = fs.readFile("./pages/toDoList.txt",function (error) {
+        if (error) throw error;
+    });
+    return data;
+}
+
 function main(request, response){
     var data;
     if (request.url==='/')
@@ -16,12 +32,22 @@ function main(request, response){
     else if (request.url==='/favicon.ico')
         data = fs.readFile('./favicon.ico',function (error, data) {
             response.end(data)});
-    else if (request.url==='/ToDo.js')
+    else if (request.url==='/ToDo.js'){
         data = fs.readFile('./pages/ToDo.js',function (error, data) {
             response.end(data)});
-    if (request.url==='/toDoList.txt'){
-        data = fs.readFile('./pages/toDoList.txt',function (error, data) {response.end(data);})
-    };
+    }
+    else if (request.url==='/toDoList.txt'){
+        console.log('Sending list');
+        data = fs.readFile('./pages/toDoList.txt',function (error, data) {
+            if (error) {fs.readFile('./pages/toDoList_Default.txt', function (error,data){
+                saveData(data);
+                response.end(data);
+            })} else
+            response.end(data);})
+    } else if (request.url==='/saveData'){
+        request.on('data',saveData);
+        response.end();
+    }
 }
 
 function startFunction(){
--- pages/ToDo.js
+++ pages/ToDo.js
@@ -1,11 +1,37 @@
 /**
  * Created by Agronom on 24.03.2018.
  */
+/*В следующей функции сохраняем все задачи в JSON
+и отправляем на сервер с URL='saveDate',
+этот url служит указанием для сервера запустить
+функцию сохранения данных
+ */
+function saveData(){
+    var data = [];
+    var toDoItems = document.getElementsByClassName('toDoItem');
+    for (var i=0;i<toDoItems.length;i++)
+    {
+        var isChecked = toDoItems.item(i).querySelector('.completeItem').checked;
+        var temp = toDoItems.item(i).querySelector('.labels').innerHTML;
+        data.push({'checked':isChecked,'task':temp});
+    }
+    data = JSON.stringify(data);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+        }
+    }
+    request.open('POST', 'saveData',true);
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send(data);
+}
 
-function makeToDoItem(title) {
+
+/*вспомогательная функция для формирования одного элемента*/
+function makeToDoItem(title,check) {
     var element = document.createElement('div');
     element.setAttribute('class','toDoItem');
-    const newToDoItem = [{tag:'input',className:'completeItem',type:'checkbox'},
+    const newToDoItem = [{tag:'input',className:'completeItem',type:'checkbox',checked:check},
                          {tag:'label',className:'labels',innerHTML:title},
                          {tag:'input',className:'changeButton',type:'button',value:'Изменить'},
                          {tag:'input',className:'deleteButton',type:'button',value:'Удалить'}]
@@ -13,7 +39,9 @@ function makeToDoItem(title) {
     for (var i=0;i<newToDoItem.length;i++) {
         var tempEl = document.createElement(newToDoItem[i]['tag']);
         Object.keys(newToDoItem[i]).forEach(function(item){
-            if (newToDoItem[i]!=='tag') tempEl[item]=newToDoItem[i][item]});
+            if (newToDoItem[i]!=='tag') tempEl[item]=newToDoItem[i][item];
+            if (newToDoItem[i].checked===true) element.classList.add('completedItem');
+        });
         element.appendChild(tempEl);
     }
     element.querySelector('.completeItem').addEventListener('click',completeToDoItem);
@@ -21,42 +49,35 @@ function makeToDoItem(title) {
     return element;
 }
 
+/*добавить задачу*/
 function addToDoItem(){
     var title = document.getElementById('inputField').value;
-    toDoList.appendChild(makeToDoItem(title));
-
+    toDoList.appendChild(makeToDoItem(title,false));
+    document.getElementById('inputField').value="";
+    saveData();
 }
 
+/*редактировать задачу*/
 function editToDoItem(){
 
 }
 
+/*удалить задачу*/
 function deleteToDoItem(){
     var toDoItem = this.parentElement;
     toDoList.removeChild(toDoItem);
+    saveData();
 }
 
+/*завершить задачу*/
 function completeToDoItem() {
     const toDoItem = this.parentElement;
     toDoItem.classList.toggle('completedItem');
+    saveData();
 }
 
-
+/*Привязка обработчиков к событиям*/
 function bindListeners(){
-    Object.keys(toDoList).forEach(function() {
-        completeItem.addEventListener('click', completeToDoItem);
-        deleteButton.addEventListener('click', deleteToDoItem);
-        addButton.addEventListener('click', addToDoItem)})
-}
-window.onload = function(){
-    var req = new XMLHttpRequest();
-    req.open("GET", "toDoList.txt", true);
-    console.log(req.status);
-    req.addEventListener("load", function() {
-        console.log("Done:", req.status);
-        console.log(req.response);
-    });
-    req.send(null);
     const completeItem = document.querySelector('.completeItem');
     const toDoList = document.querySelector('#toDoList');
     const deleteButton = document.querySelector('.deleteButton');
@@ -64,4 +85,26 @@ window.onload = function(){
     completeItem.addEventListener('click', completeToDoItem);
     deleteButton.addEventListener('click', deleteToDoItem);
     addButton.addEventListener('click',addToDoItem);
+    /*Object.keys(toDoList).forEach(function() {
+        completeItem.addEventListener('click', completeToDoItem);
+        deleteButton.addEventListener('click', deleteToDoItem);
+        addButton.addEventListener('click', addToDoItem)})*/
+}
+
+function loadToDoList(data) {
+    var temp;
+    data = JSON.parse(data);
+    data.forEach(function(item){toDoList.appendChild(makeToDoItem(item.task,item.checked))});
+}
+
+window.onload = function(){
+    var req = new XMLHttpRequest();
+    var data;
+    req.open("POST", "toDoList.txt", true);
+    req.setRequestHeader('Content-type','application/json; charset=utf8')
+    req.addEventListener("load", function() {
+        loadToDoList(req.responseText);
+        bindListeners();
+    });
+    req.send();
 }
